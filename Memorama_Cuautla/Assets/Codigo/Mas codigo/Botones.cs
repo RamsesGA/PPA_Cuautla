5ff8e090567065b03850f487580c33ae011b2b8a
LAPTOP-BGAV0320\ramse <idv18c.rguerrero@uartesdigitales.edu.mx>
2019-10-05 22:34:18 -0500

    //Leves cambios

---
+++ Memorama_Cuautla/Assets/Codigo/Mas codigo/Botones.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Botones : MonoBehaviour {
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+    public void Jugar(string SceneName)
+    {
+        SceneManager.LoadScene(2);
+    }
+ 
+}
---
+++ Memorama_Cuautla/Assets/Escenas/Escena_Ramses/Codigos/Transiciones_escena.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Transiciones_escena : MonoBehaviour
+{
+    public Animator transition_Anim;
+    public string scene_name;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(LoadScene());
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        transition_Anim.SetTrigger("Fin");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(scene_name);
+    }
+}
