5f1626fe5c4c47a6e474d2e23252e415be2e3210
LAPTOP-BGAV0320\ramse <idv18c.rguerrero@uartesdigitales.edu.mx>
2019-11-13 09:56:08 -0600

    //Implementación de arte y scroll B_G
    B_G: BackGround

---
+++ Memorama_Cuautla/Assets/Codigo/Mas codigo/Scroll_fondo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll_fondo : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    public float scrollSpeed = 1.0f;
+
+    private void Awake(){
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Start(){
+        rigidBody2D.velocity = new Vector2(scrollSpeed, 0);
+    }
+
+    void Update(){
+    }
+}
